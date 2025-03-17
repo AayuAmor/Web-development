0e2aa72df135a0dfe97adc41a6657c4287b2672d
AayuAmor <aayuamor@gmail.com>
2025-03-17 04:25:15 +0000

    Adding Function JS file

---
+++ JavaScript/Functions.js
@@ -0,0 +1,15 @@
+//sum function
+function sum (a, b){
+    return a+b;
+}
+//Modern JS = ArrowFunction
+const arrowSum = (a, b) =>{
+    console.log(a+b);
+};
+
+function mul(a,b){
+    return a*b;
+}
+const arrowMul=(a, b) => {
+    console.log(a*b);
+};
